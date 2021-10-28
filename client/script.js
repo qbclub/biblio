0269dc823a0ed37baf24f2042ecc60ba51b47ba0
grachevrv <grachevrv@ya.ru>
2021-10-28 23:48:34 +0400

    data draw

--- client/script.js
+++ client/script.js
@@ -1,13 +1,24 @@
-var container = document.getElementById("container");
-
 $("button").click(giveMe)
+
+let table = document.getElementById("table")
 
 function giveMe() {
 
     let request = $.get("https://localhost:5001/weatherforecast");
 
     request.done(function (data) {
-        let response = JSON.parse(data);
-        alert(response)
+        data.forEach(element => {
+            let row = document.createElement("tr");
+            for (var key in element) {
+
+                let cell = document.createElement("td")
+                cell.innerHTML = element[key]
+                row.appendChild(cell)
+                table.appendChild(row);
+            }
+
+            console.log(element)
+        });
+
     });
 }
